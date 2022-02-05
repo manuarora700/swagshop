888356ed8ff44e075b3edea4137963eca3ff7c4b
Manu Arora <manuarorawork@gmail.com>
2022-02-05 23:04:09 +0530

    ⚡️ Init frontend

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# 🤩 Swagshop - The only e-commerce store you need to practice your MERN Skills.
---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+const App = () => {
+  return (
+    <div className="App">
+      <div className="flex justify-center space-x-1 mt-4">
+        <span className="text-4xl">🤩</span>
+        <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-black to-[#4c4c4d] font-bold text-4xl">
+          Swagshop
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
