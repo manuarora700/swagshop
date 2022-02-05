23e07d682347f2001c2850c0ae042ed7c9a53dfa
Manu Arora <manuarorawork@gmail.com>
2022-02-06 01:15:33 +0530

    Card Designs

---
+++ frontend/src/components/ProductCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ProductCard = ({ product }) => {
+  return (
+    <Link to={`/product/${product._id}`} className="relative">
+      <div className="absolute bg-red-500 rounded-md z-10 px-2 py-1 text-white font-bold -top-2 -left-2 transform -rotate-2">
+        {product.brand}
+      </div>
+      <div className="rounded-md product-card p-2 group overflow-hidden h-full">
+        <div className="relative">
+          <img
+            src={product.image}
+            alt={product.name}
+            className="w-full rounded-md transform group-hover:scale-110 transition duration-[4000ms] ease-in-out"
+          />
+        </div>
+
+        <div className="px-4">
+          <h1 className="my-2 font-semibold text-2xl">{product.name}</h1>
+          <p className="my-4 font-normal  text-sm text-gray-600 ">
+            {product.rating} ⭐️ from {product.numReviews} reviews
+          </p>
+          <span className="inline-block my-4 font-normal text-xl rounded-md px-4 py-1 bg-gradient-to-r from-black to-[#272727] text-white">
+            ${product.price}
+          </span>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default ProductCard;
--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,17 +1,24 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import ProductCard from "../components/ProductCard";
+import products from "../constants/products";
 
 const Landing = () => {
   return (
     <div>
       <Header />
-      <main className="max-w-7xl mx-auto h-[80vh]">
-        <div className="flex justify-center space-x-1 mt-4 items-center antialiased">
+      <main className="max-w-7xl mx-auto">
+        <div className="flex space-x-1 mt-4 justify-center antialiased mb-14">
           <span className="text-4xl">🤩</span>
           <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-black to-[#4c4c4d] font-bold text-4xl">
-            Swagshop
+            Latest Products
           </h1>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20  mx-auto">
+          {products.map((product) => (
+            <ProductCard product={product} key={product._id} />
+          ))}
         </div>
       </main>
       <Footer />
